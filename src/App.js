81d25fcdd6bd31a38e8c36e23c6bac60de3b2a8c
juna <ermanjunaidi1@gmail.com>
2018-12-01 17:23:05 +0700

    Ganti Negara

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <div>Erman Junaidi Jawa Tengah</div>
+            <div>Erman Junaidi Indonesia</div>
           </p>
           <a
             className="App-link"
